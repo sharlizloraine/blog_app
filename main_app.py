b00f8cbbf0ea2095d42b4f8028ac913e79186e34
sharlizloraine <16073@my.sanctamaria.school.nz>
2022-02-15 14:31:25 +1300

    creating posts

--- main_app.py
+++ main_app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
+posts = [
+    
+    {
+        'author': 'Sharliz Ang',
+        'title': 'Blog Post 1',
+        'content': 'First post content', 
+        'date posted': 'February 2 2022',
+    }, 
+    
+    {
+        'author': 'Sharliz Ang',
+        'title': 'Blog Post 2',
+        'content': 'Second post content', 
+        'date posted': 'February 14 2022',
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
