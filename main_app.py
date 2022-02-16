4b1496f47bfa5ba0486da0060d5e26c5a10fb041
sharlizloraine <16073@my.sanctamaria.school.nz>
2022-02-17 09:51:58 +1300

    adding bootstrap

--- main_app.py
+++ main_app.py
@@ -15,17 +15,29 @@ posts = [
         'title': 'Blog Post 2',
         'content': 'Second post content', 
         'date posted': 'February 14 2022',
+    },
+    
+    {
+        'author': 'Sharliz Ang',
+        'title': 'Blog Post 3',
+        'content': 'Third post content', 
+        'date posted': 'February 14 2022',
     }
 ]
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
+# Routes 
 
+# Route for homepage
+@app.route("/") # Route for main page 
+@app.route("/home") # Route to home page 
+def home():
+    return render_template('home.html', posts=posts) # Call to render the home page template 
+
+# Route for about page 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
+# Allows main app to run the web server
 if __name__ == '__main__':
     app.run(debug=True)
